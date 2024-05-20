31f43ca0ceb091d6cb4543929f87ac0ae49689ae
DipoFada <92977411+DipoFada@users.noreply.github.com>
2024-05-20 06:10:22 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,14 @@
-#importing the libraries
+#importing the python libraries
+import re
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import nltk
+import spacy
+import wordcloud
+import gensim
+from gensim.utils import simple_preprocess
+
+#reading the data
